32ae30991ad730feeddb1c5b2ea86b34f6e180f7
sosaspeaks <165622402+sosaspeaks@users.noreply.github.com>
2024-08-30 16:19:52 -0500

    dunno if this will fix anthing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,41 @@
 // Filter Js
 $(document).ready(function(){
-    $('.filter-item').click(function() {
-        const value = $(this).attr('data-filter');
-        if (value == 'all') {
-            $('.post-box').show('1000');
-        }
-        else{
-        $('.post-box')
-            .not("." + value)
-            .hide('1000')
-        $('.post-box')
-        .filter('.' + value)
-        .show('1000');
-        }
-    });
-    // Add active to btn
-    $('.filter-item').click(function(){
-        $(this).addClass('active-filter').siblings().removeClass('active-filter');
-    })
+  $('.filter-item').click(function() {
+      const value = $(this).attr('data-filter');
+      if (value == 'all') {
+          $('.post-box').show('1000');
+      }
+      else{
+      $('.post-box')
+          .not("." + value)
+          .hide('1000')
+      $('.post-box')
+      .filter('.' + value)
+      .show('1000');
+      }
+  });
+  // Add active to btn
+  $('.filter-item').click(function(){
+      $(this).addClass('active-filter').siblings().removeClass('active-filter');
+  })
 });
 
 //Header Background Change On Scroll
 let header = document.querySelector('header')
 
 window.addEventListener('scroll', () => {
-    header.classList.toggle('shadow', window.scrollY > 0)
+  header.classList.toggle('shadow', window.scrollY > 0)
 });
 
 // Loader
 window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
+  const loader = document.querySelector(".loader");
 
-    loader.classList.add("loader--hidden");
+  loader.classList.add("loader--hidden");
 
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild(loader);
-    });
+  loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+  });
 });
 
 // // Dark Mode
@@ -51,39 +51,39 @@ let darkMode = localStorage.getItem('darkMode');
 const themeToggleBtn = document.querySelector('.theme-toggle');
 
 const enableDarkMode = () => {
-  // 1. Add the class to the body
-  document.body.classList.add('dark-mode');
-  themeToggleBtn.classList.add('bx-sun');
-  // 2. Update darkMode in localStorage
-  localStorage.setItem('darkMode', 'enabled');
+// 1. Add the class to the body
+document.body.classList.add('dark-mode');
+themeToggleBtn.classList.add('bx-sun');
+// 2. Update darkMode in localStorage
+localStorage.setItem('darkMode', 'enabled');
 }
 
 const disableDarkMode = () => {
-  // 1. Remove the class from the body
-  document.body.classList.remove('dark-mode');
-  themeToggleBtn.classList.remove('bx-sun');
-  // 2. Update darkMode in localStorage 
-  localStorage.setItem('darkMode', null);
+// 1. Remove the class from the body
+document.body.classList.remove('dark-mode');
+themeToggleBtn.classList.remove('bx-sun');
+// 2. Update darkMode in localStorage 
+localStorage.setItem('darkMode', null);
 }
- 
+
 // If the user already visited and enabled darkMode
 // start things off with it on
 if (darkMode === 'enabled') {
-  enableDarkMode();
+enableDarkMode();
 }
 
 // When someone clicks the button
 themeToggleBtn.addEventListener('click', () => {
-  // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode'); 
-  
-  // if it not current enabled, enable it
-  if (darkMode !== 'enabled') {
-    enableDarkMode();
-  // if it has been enabled, turn it off  
-  } else {  
-    disableDarkMode(); 
-  }
+// get their darkMode setting
+darkMode = localStorage.getItem('darkMode'); 
+
+// if it not current enabled, enable it
+if (darkMode !== 'enabled') {
+  enableDarkMode();
+// if it has been enabled, turn it off  
+} else {  
+  disableDarkMode(); 
+}
 });
 
 
@@ -91,14 +91,14 @@ themeToggleBtn.addEventListener('click', () => {
 
 const toTop = document.querySelector(".to-top");
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 50) {
-        toTop.classList.add("active");
-    } else {
-        toTop.classList.remove("active");
-    }
+  if(window.scrollY > 50) {
+      toTop.classList.add("active");
+  } else {
+      toTop.classList.remove("active");
+  }
 })
 
 toTop.addEventListener('click', () => {
-    document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 )
